a09e162822ab3fcd7c76e46b5fa6ae7db40a54da
Mamtha Nagarajan <Mamtha.Nagarajan@contractor.tranetechnologies.com>
2023-07-20 20:35:29 +0530

    terraform output10

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -8,7 +8,7 @@ terraform {
    backend "azurerm" {
       resource_group_name  = "Ansible"      
       storage_account_name =  "ansiblestorage1234"    
-      container_name       =  "terraform"
+      container_name       =  "teraform"
       key                  = "terraform.tfstate"
   }
 }
